abb18b4972fce5cba3016c34681fb64a52eb2ea5
Kaarshnav <rajneeshupreti98@gmail.com>
2025-11-25 13:55:49 +0530

    ✅ added signup and login apis and addd password encryption , schema validation , data sanitization

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,21 +1,71 @@
 const connectDB = require("./config/database");
 const express = require("express");
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 const User = require("./models/user");
+
 app.use(express.json());
 // app.use("/", (req, res) => {
 //   console.log(" at / path ");
 //   res.send(" --hello from use of expreess -");
 // });
 
+app.get("/getUser", async (req, res) => {
+  try {
+    const emailId = req.body.email;
+    const users = await User.find({ email: emailId });
+    if (users.length === 0) res.send(404).send("user not found");
+    else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.send(400).send("something went wrong");
+  }
+});
+app.get("/getAllUser", async (req, res) => {
+  try {
+    const emailId = req.body.email;
+    const users = await User.find({});
+    if (users.length === 0) res.send(404).send("user not found");
+    else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.send(400).send("something went wrong");
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(validator.isEmail(email), " -- res---");
+    if (!validator.isEmail(email)) {
+      throw new Error(" please add valid email id ");
+    }
+    const userFromDb = await User.findOne({ email });
+    if (!userFromDb) throw new Error(" user not found with that mail id  ");
+    const isCoorectPass = await bcrypt.compare(password, userFromDb.password);
+    if (isCoorectPass) res.send(" User suggeesfully loggedin");
+    else {
+      throw new Error(" incoreect pass ");
+    }
+  } catch (err) {
+    res.status(404).send(" Invalid creds " + err.message);
+  }
+});
 app.post("/signup", async (req, res) => {
   try {
     // gettig data from query params
     // console.log(req.query);
+
+    //only gettin. what's required
+    const { firstName, lastName, email, password } = req.body;
+    const hashPass = await bcrypt.hash(password, 10); // 10 round of encrpt
+    console.log(" passed pass ---", password, "-- hash pass ---", hashPass);
     console.log(req.body);
     // creating user from that
     // const firstUser = new User({
@@ -30,7 +80,12 @@ app.post("/signup", async (req, res) => {
     //   firstName: req.query.firstName,
     //   lastName: req.query.lastName,
     // });
-    const userFromBody = User(req.body);
+    const userFromBody = User({
+      firstName,
+      lastName,
+      email,
+      password: hashPass,
+    });
     // saving data to db
     // await mongodb.create(firstUser);
     // await firstUser.save();
--- devTinder/src/models/user.js
+++ devTinder/src/models/user.js
@@ -1,15 +1,42 @@
 // schema for chai drinker user
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    require: true,
-    minlength: 3,
+
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      minLength: 3,
+      maxLength: 15,
+      trim: true,
+    },
+    lastName: { type: String, trim: true },
+    noOfTeaPerDay: {
+      type: Number,
+      maxLength: 10,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate: (email) => {
+        return validator.isEmail(email);
+      },
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+    },
   },
-  lastName: { type: String },
-  noOfTeaPerDay: {
-    type: Number,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 // this model rename wasted 1 hr so make sure the db name is matching at third arg
 module.exports = mongoose.model("User", userSchema, "chaiDrinkers");

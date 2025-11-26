47907f601269d1694086d260efd30f467a523425
Kaarshnav <rajneeshupreti98@gmail.com>
2025-11-26 18:52:01 +0530

    ✅ Added review connection API's

--- devTinder/src/routes/request.js
+++ devTinder/src/routes/request.js
@@ -3,6 +3,7 @@ const requestRouter = express.Router();
 const auth = require("../middlewares/auth");
 const User = require("../models/user");
 const ConnectionRequestModel = require("../models/connectionRequest");
+
 requestRouter.post("/sendConnectionRequest/:status", auth, async (req, res) => {
   // this is fr sending req and replying to recieved req ,when both are interested then only connection is made
 
@@ -37,5 +38,42 @@ requestRouter.post("/sendConnectionRequest/:status", auth, async (req, res) => {
     res.status(400).send(" Something went wrong " + err.message);
   }
 });
+requestRouter.patch(
+  "/reviewConnectionRequest/:status/:id",
+  auth,
+  async (req, res) => {
+    // user will review the recieved  connenction request , so allowed ststus will be rejected accepted , not ignore,
+    //  coz that is while sending req, asume in likes section of tinder
+    // getting user from userId via query params ,not from body( just to check that apth as well )
+
+    // validation for status and userId
+    try {
+      const allowedStatus = ["Accepted", "Rejected"];
+      if (!allowedStatus.includes(req.params.status)) {
+        throw new Error(" Status is not valid");
+      }
+      // toId is always from userData.id i.e logged in user can only his data
+      const loggedInUser = req.userData;
+      const connectionUser = await ConnectionRequestModel.findOne({
+        fromId: req.params.id,
+        toId: loggedInUser._id,
+      });
+      console.log("first", loggedInUser._id, req.params.id, connectionUser);
+      if (!connectionUser) {
+        throw new Error(" connection to that user doesn't exist");
+      }
+      await connectionUser.updateOne({
+        fromId: req.params.id,
+        toId: loggedInUser._id,
+        status: req.params.status,
+      });
+      res.json({
+        message: ` status updated for connection from ${req.params.id} to ${loggedInUser._id} as  ${req.params.status} by ${loggedInUser.firstName}`,
+      });
+    } catch (err) {
+      res.status(400).json({ message: "smething went wrong " + err.message });
+    }
+  }
+);
 
 module.exports = requestRouter;
